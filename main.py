79910693f152a551280c40e0237b985c22fa3330
pridethedaemon <lp2001@hw.ac.uk>
2020-12-06 00:29:38 +0000

    apparently i did something…

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ import sys
 import json
 import pandas
 
-data = []
-data = pandas.read_json(open('./issuu_sample.json'), lines=True)
+uuid = sys.argv[1]
+
+df = pandas.read_json(open('./issuu_sample.json'), lines=True)
+
+df_document = df[df.env_doc_id == uuid]
+
+df_countries = df['visitor_country'].value_counts()
+
+refined_useragent = df_document.visitor_useragent.str.extract(r'(\w+)/.*$')
+df_document = df_document.assign(visitor_useragent=refined_useragent)
+df_browsers = df_document['visitor_useragent'].value_counts()
+
+print(df_document)
+print(df_countries)
+print(df_browsers)
