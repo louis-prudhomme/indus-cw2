56e3fbcc1793f3f1cdede7b03c2df8054ed4ab33
pridethedaemon <lp2001@hw.ac.uk>
2020-12-06 04:22:28 +0000

    Don’t even try to refactor it.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 import sys
 import json
 import pandas
+import numpy
 import matplotlib.pyplot as pyplot
 
 uuid = sys.argv[1]
 
-df_global = pandas.read_json(open('./issuu_sample.json'), lines=True)
-df_document = df_global[df_global.env_doc_id == uuid]
+df_global = pandas.read_json(open('./issuu_cw2.json'), lines=True)
+df_document = df_global[df_global.subject_doc_id == uuid]
 
 def get_countries(df_document):
     return df_document.visitor_country.value_counts()
@@ -25,12 +26,23 @@ def get_top10_readers(df_global):
     return df_pageandread.groupby(['visitor_uuid']).sum().sort_values('event_readtime').head(10)
 
 def get_readers_uuids(df_global, doc_uuid):
-    return df_global[df_global.env_doc_id == doc_uuid].visitor_uuid.unique()
+    return pandas.Series(df_global[df_global.subject_doc_id == doc_uuid].visitor_uuid.unique())
 
-def get_docs_read(df_global, user_uuid):
-    return df_global[df_global.visitor_uuid == user_uuid].env_doc_id.unique()
+def get_docs_read(df_global, visitor_uuid):
+    return pandas.Series(df_global[df_global.visitor_uuid == visitor_uuid].subject_doc_id)
 
-def get_like(df_global):
-    return 0
-    
-get_countries(df_document).plot.bar()
+def get_alike(df_global, doc_uuid, user_uuid=0):
+    df_read = df_global[df_global.event_type == 'read']
+    sr_readers = get_readers_uuids(df_global, doc_uuid)
+    sr_readers = sr_readers[sr_readers != user_uuid]
+
+    list_series = []
+    for usr in sr_readers:
+        list_series.append(get_docs_read(df_global, usr))
+
+    sr_alike = pandas.concat(list_series)
+    sr_alike.dropna(inplace=True)
+    return sr_alike[sr_alike != doc_uuid]
+
+print(get_alike(df_global, uuid))
+print(get_alike(df_global, uuid).value_counts())
