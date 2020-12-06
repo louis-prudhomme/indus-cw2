c34195b7721a90071eaa7cfdd05e6f5b9cccdc44
pridethedaemon <lp2001@hw.ac.uk>
2020-12-06 10:58:03 +0000

    This is the last time we let Tom commit ascii porn in the comments.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ df_global = pandas.read_json(open('./issuu_cw2.json'), lines=True)
 df_document = df_global[df_global.subject_doc_id == uuid]
 
 def get_countries(df_document):
-    return df_document.visitor_country.value_counts().rename_axis('visitor_country').reset_index(name='number_visitors')
+    return df_document.visitor_country.value_counts().rename_axis('visitor_country').to_frame('number_visitors')
 
 def get_continents(df_countries):
     return 0
@@ -20,7 +20,7 @@ def get_browsers(df_global):
     refined_useragent = df_global.visitor_useragent.str.extract(r'(\w+)/.*$')
     df_global = df_global.assign(visitor_useragent=refined_useragent)
     
-    return df_global.visitor_useragent.value_counts().rename_axis('visitor_browser').reset_index(name='number_visitors')
+    return df_global.visitor_useragent.value_counts().rename_axis('visitor_browser').to_frame('number_visitors')
 
 def get_top10_readers(df_global):
     df_pageandread = df_global[df_global.event_type == 'pagereadtime'][['visitor_uuid', 'event_readtime']]
