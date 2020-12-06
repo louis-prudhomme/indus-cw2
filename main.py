6b6d81847a7f98a7e2aebec7e084a3cac746fe79
pridethedaemon <lp2001@hw.ac.uk>
2020-12-06 12:04:17 +0000

    I don't believe it

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pandas
 import numpy
 import matplotlib.pyplot as pyplot
 
+import tkinter as tk
+
 uuid = sys.argv[1]
 
 df_global = pandas.read_json(open('./issuu_cw2.json'), lines=True)
@@ -13,16 +15,16 @@ df_continents = pandas.read_json(open('./continent.json'), typ='series').rename_
 df_document = df_global[df_global.subject_doc_id == uuid]
 
 def get_countries(df_document):
-    return df_document.visitor_country.value_counts().rename_axis('visitor_country').reset_index(name='number_visitors')
+    return df_document.visitor_country.value_counts().rename_axis('visitor_country').reset_index(name='number_visitors').set_index('visitor_country')
 
 def get_continents(df_document, df_continents):
-    return pandas.merge(left=df_document, right=df_continents,  left_on='visitor_country', right_on='country_code').continent_code.value_counts().rename_axis('visitor_continent').reset_index(name='number_visitors')
+    return pandas.merge(left=df_document, right=df_continents,  left_on='visitor_country', right_on='country_code').continent_code.value_counts().rename_axis('visitor_continent').reset_index(name='number_visitors').set_index('visitor_continent')
 
 def get_browsers(df_global):
     refined_useragent = df_global.visitor_useragent.str.extract(r'(\w+)/.*$')
     df_global = df_global.assign(visitor_useragent=refined_useragent)
     
-    return df_global.visitor_useragent.value_counts().rename_axis('visitor_browser').reset_index(name='number_visitors')
+    return df_global.visitor_useragent.value_counts().rename_axis('visitor_browser').reset_index(name='number_visitors').set_index('visitor_browser')
 
 def get_top10_readers(df_global):
     df_pageandread = df_global[df_global.event_type == 'pagereadtime'][['visitor_uuid', 'event_readtime']]
@@ -57,15 +59,27 @@ def also_like(df_global, doc_uuid, user_uuid=0, sort_func=lambda df: df):
     return sort_func(get_alike(df_global, doc_uuid, user_uuid, sort_func).head(10))
 
 def make_plot(df, title, label_x, label_y):
-    df.plot(kind='bar')
+    df.plot(kind='bar', rot=0)
     pyplot.suptitle(title)
     pyplot.xlabel(label_x)
     pyplot.ylabel(label_y)
     pyplot.show()
 
-make_plot(get_countries(df_document),'Number of visitors per country', 'Countries','Visitors')
-make_plot(get_browsers(df_global),'Number of visitors per browser', 'Browsers','Visitors')
-make_plot(get_top10_readers(df_global), 'Most avid readers','Readers','Time read')
-make_plot(get_continents(df_document, df_continents), 'Number of visitors per continent', 'Continents', 'Visitors')
-print(get_continents(df_document, df_continents))
-print(get_countries(df_document))
+#make_plot(get_continents(df_document, df_continents), 'Number of visitors per continent', 'Continents', 'Visitors')
+#make_plot(get_countries(df_document.copy()),'Number of visitors per country', 'Countries','Visitors')
+#make_plot(get_browsers(df_global.copy()),'Number of visitors per browser', 'Browsers','Visitors')
+#make_plot(get_top10_readers(df_global.copy()), 'Most avid readers','Readers','Time read')
+
+window = tk.Tk()
+label_doc = tk.Label(text="User-chosen document UUID :" + uuid)
+label_doc.pack()
+label_usr = tk.Label(text="User-chosen user UUID :" + uuid)
+label_usr.pack()
+
+bt_countries = tk.Button(text="Visitors by countries", command=make_plot(get_countries(df_document.copy()),'Number of visitors per country', 'Countries','Visitors'))
+bt_countries.pack()
+
+bt_continents = tk.Button(text="Visitors by countries", command=make_plot(get_continents(df_document, df_continents), 'Number of visitors per continent', 'Continents', 'Visitors'))
+bt_continents.pack()
+
+window.mainloop()
