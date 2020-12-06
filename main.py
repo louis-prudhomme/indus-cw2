283c56390ec0b84e9ee7a3cfad32281e7b2155f2
pridethedaemon <lp2001@hw.ac.uk>
2020-12-06 11:26:45 +0000

    s/import/include/

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ df_global = pandas.read_json(open('./issuu_cw2.json'), lines=True)
 df_document = df_global[df_global.subject_doc_id == uuid]
 
 def get_countries(df_document):
-    return df_document.visitor_country.value_counts().rename_axis('visitor_country').to_frame('number_visitors')
+    return df_document.visitor_country.value_counts().rename_axis('visitor_country').reset_index(name='number_visitors')
 
 def get_continents(df_countries):
     return 0
@@ -20,7 +20,7 @@ def get_browsers(df_global):
     refined_useragent = df_global.visitor_useragent.str.extract(r'(\w+)/.*$')
     df_global = df_global.assign(visitor_useragent=refined_useragent)
     
-    return df_global.visitor_useragent.value_counts().rename_axis('visitor_browser').to_frame('number_visitors')
+    return df_global.visitor_useragent.value_counts().rename_axis('visitor_browser').reset_index(name='number_visitors')
 
 def get_top10_readers(df_global):
     df_pageandread = df_global[df_global.event_type == 'pagereadtime'][['visitor_uuid', 'event_readtime']]
@@ -54,27 +54,13 @@ def sort_df_desc(df_alike):
 def also_like(df_global, doc_uuid, user_uuid=0, sort_func=lambda df: df):
     return sort_func(get_alike(df_global, doc_uuid, user_uuid, sort_func).head(10))
 
-fig, axes = pyplot.subplots(nrows=2, ncols=2)
-ax0, ax1, ax2, ax3 = axes.flatten()
+def make_plot(df, title, label_x, label_y):
+    df.plot(kind='bar')
+    pyplot.suptitle(title)
+    pyplot.xlabel(label_x)
+    pyplot.ylabel(label_y)
+    pyplot.show()
 
-ax0.hist(get_countries(df_document.copy()))
-ax0.set_title("Number of visitors by country")
-ax0.set_xlabel("Country")
-ax0.set_ylabel("Number of visitors")
-
-ax1.hist(get_countries(df_document.copy()))
-ax1.set_title("Number of visitors by continent")
-ax1.set_xlabel("Continent")
-ax1.set_ylabel("Number of visitors")
-
-ax2.hist(get_browsers(df_global.copy()))
-ax2.set_title("Number of visitors by brower")
-ax2.set_xlabel("Browser")
-ax2.set_ylabel("Number of visitors")
-
-ax3.hist(get_top10_readers(df_global.copy()))
-ax3.set_title("Most avid readers")
-ax3.set_xlabel("Reader")
-ax3.set_ylabel("Number of reads")
-
-print(get_countries(df_document.copy()))
+make_plot(get_countries(df_document.copy()),'Number of visitors per country', 'Countries','Visitors')
+make_plot(get_browsers(df_global.copy()),'Number of visitors per browser', 'Browsers','Visitors')
+make_plot(get_top10_readers(df_global.copy()), 'Most avid readers','Readers','Time read')
