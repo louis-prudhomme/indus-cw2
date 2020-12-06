b2c3f0830df90cbf42f839ef60d0f8c805467ba8
pridethedaemon <lp2001@hw.ac.uk>
2020-12-06 01:27:12 +0000

    Add Sandbox

--- main.py
+++ main.py
@@ -24,27 +24,13 @@ def get_top10_readers(df_global):
     df_pageandread = df_global[df_global.event_type == 'pagereadtime'][['visitor_uuid', 'event_readtime']]
     return df_pageandread.groupby(['visitor_uuid']).sum().sort_values('event_readtime').head(10)
 
-fig, axes = pyplot.subplots(nrows=2, ncols=2)
-ax0, ax1, ax2, ax3 = axes.flatten()
+def get_readers_uuids(df_global, doc_uuid):
+    return df_global[df_global.env_doc_id == doc_uuid].visitor_uuid.unique()
 
-ax0.hist(get_countries(df_document))
-ax0.set_title("Number of visitors by country")
-ax0.set_xlabel("Country")
-ax0.set_ylabel("Number of visitors")
+def get_docs_read(df_global, user_uuid):
+    return df_global[df_global.visitor_uuid == user_uuid].env_doc_id.unique()
 
-ax1.hist(get_countries(df_document))
-ax1.set_title("Number of visitors by continent")
-ax1.set_xlabel("Country")
-ax1.set_ylabel("Number of visitors")
-
-ax2.hist(get_browsers(df_global))
-ax2.set_title("Number of visitors by browser")
-ax2.set_xlabel("Browser")
-ax2.set_ylabel("Number of visitors")
-
-ax3.hist(get_top10_readers(df_global))
-ax3.set_title("Top 10 avid readers")
-ax3.set_xlabel("Browser")
-ax3.set_ylabel("Number of visitors")
-
-pyplot.show()
+def get_like(df_global):
+    return 0
+    
+get_countries(df_document).plot.bar()
